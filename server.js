0f16cff3c5c0abbca3b435345d5ac0d04ed6de23
Alvaro <aleiva97@gmail.com>
2022-08-31 21:47:26 -0400

    back entregable

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ const numCPUs = require('os').cpus().length
 
 const yargs = require('yargs/yargs')(process.argv.slice(2))
 
+console.log(yargs.argv.dev)
 
 
 const {puerto,cluster} = yargs.
@@ -83,17 +84,34 @@ if(cluster && clusterOn.isMaster){
 }else {
   console.log('fork')
 
-  // const PORT = yargs.argv.puerto || process.env.PORT  || 8080   DESAROLLO
+   if(yargs.argv.dev){
 
-  const PORT =  process.env.PORT  || 8080 // PRODUCCION
+       const PORT = yargs.argv.puerto || process.env.PORT  || 8080  
 
-  app.get('/fork' , (req,res) => {
-      res.send(`servidor express en ${PORT} - PID ${process.pid}  `)
-  } )
+          app.get('/fork' , (req,res) => {
+            res.send(`servidor express en ${PORT} - PID ${process.pid}  `)
+        } )
 
-  server.listen(PORT , err => {
-      if(!err) console.log(`servidor express en ${PORT} - PID ${process.pid}  `)
-  })
+        server.listen(PORT , err => {
+            if(!err) console.log(`servidor express en ${PORT} - PID ${process.pid}  `)
+        })
+
+         
+   }else {
+
+    const PORT =  process.env.PORT  || 8080 // PRODUCCION
+
+    app.get('/fork' , (req,res) => {
+        res.send(`servidor express en ${PORT} - PID ${process.pid}  `)
+    } )
+  
+    server.listen(PORT , err => {
+        if(!err) console.log(`servidor express en ${PORT} - PID ${process.pid}  `)
+    })
+
+
+   }   
+
 
 
 }
@@ -204,85 +222,4 @@ app.get('/home/:email', cors(),async (req,res) => {
 })
 
 
-// app.post('/finalizar', async (req,res) => {
 
-//   const productos = req.body.carrito.map(({nombre,precio}) => {
-//     return {nombre, precio}
-//   } )
-
-// console.log(productos)
-//             let transporter = nodemailer.createTransport({
-//               service:"gmail",
-//               auth:{
-//               user:"alvaro.leiva@websal.com",
-//               pass:"alvaroagustin1997"
-//               },
-//               tls:{
-//               rejectUnauthorized: false
-//               }
-//           })
-
-//           let mailOptions = {
-//             from:"Ecommerce <alvaro.leiva@websal.com>",
-//             to:'alvaro.leiva@websal.com',
-//             subject:`Nuevo pedido de ${req.body.nombre} `,
-//             text:`el usuario de email: ${req.body.email}
-//               a comprado los siguientes productos ${JSON.stringify(productos)}
-              
-//             `,
-            
-//           }
-
-
-//           transporter.sendMail(mailOptions, function (err, success) {
-//             if(err){
-//             console.log(err)
-            
-
-//             } else {
-//             console.log('Email good sent')
-//             res.json('enviado')
-//             }
-
-//           } )
-
-//           const accountSid = 'AC979e1c0006537ff58a80ad5713046c50'
-//           const authToken = 'c830bf15df3f90b3943ae27ff588b8b3'
-
-//           const client = twilio(accountSid,authToken)
-
-//           try {
-//             const message = await client.messages.create({
-//               body:'Tu pedido a sido recibido y se encuentra en proceso',
-//               from:'+19894479605',
-//               to:`+${req.body.telefono}`
-//             })
-
-//             // console.log(message)
-            
-//           } catch (error) {
-//             console.log(error)
-//             logger.error('error al iniciar sesion')
-//           }
-//           try {
-//             const message = await client.messages.create({
-//               body:`el usuario de email: ${req.body.email}
-//               a comprado los siguientes productos ${JSON.stringify(productos[0])}
-//             `,
-//               from:'whatsapp:+14155238886',
-//               to:`whatsapp:+56962121886`
-//             })
-
-//             // console.log(message)
-            
-//           } catch (error) {
-//             console.log(error)
-//             logger.error('error al iniciar sesion')
-//           }
-
-
-//           res.json('enviado')
- 
-
-
-// })
